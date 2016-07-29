9bdd93d0acaa24656ec662fe737b0ac0ae90048d
Chris Roper <chris@roper.xyz>
2016-07-29 16:25:44 -0600

    CreateTodo model, controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
+has_many :item
+
 protected
   def confirmation_required?
     false
--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,13 @@ Rails.application.routes.draw do
   get 'welcome/tasks'
   get 'welcome/index'
 
-  resources :users
-
   root 'users#show'
 
   get 'users/show'
 
-resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users do
+    resources :items, only: [:create]
+  end
 end
+
+#end
