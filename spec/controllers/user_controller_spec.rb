8c7ef3ea5bb9257dec4976ef55e351a0686a1aa2
Chris Roper <chris@roper.xyz>
2016-08-05 07:57:30 -0600

    user controller spec

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe ItemsController, type: :controller do
-  include Devise::Test::ControllerHelpers
+  #include Devise::Test::ControllerHelpers
 
-  
-   describe "GET show" do
 
+   describe "GET #Show" do
+      it "assigns the current_user to @user"
+         user = create(:user)
+         get :show, user_id: user
+         expect(assigns(:user)).to eq user
+      it "assigns the @user.items to @items"
+         user.item = create(:item)
+         get :show, id: item
+         expect(assigns(:user.items)).to eq items
    end
+ end
