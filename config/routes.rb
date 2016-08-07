f6b8c8d3a0a530f184f3affac519db7d4b9638a1
Chris Roper <chris@roper.xyz>
2016-08-07 09:55:54 -0600

    route fixes

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
 
   root 'users#show'
 
-devise_for :users
-  resources :users do
-    resources :items
+  
+  resources :users, only: [:show] do
+    resources :items, only: [:create, :destroy]
   end
 end
