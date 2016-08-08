39964a896e3300f9c15286eaa33dc5695978c646
Chris Roper <chris@roper.xyz>
2016-08-08 12:08:47 -0600

    tests

--- spec/model/item_spec.rb
+++ spec/model/item_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-describe Item do
-it {should belong_to :user}
+RSpec.describe Item, type: :model do
+  let(:user) { FactoryGirl.create :user }
+  let(:item) { FactoryGirl.create :item }
+
+  it { is_expected.to belong_to(:user) }
+
+  describe "attributes" do
+    it "has name and exp attributes" do
+      expect(item).to have_attributes(name: item.name)
+      expect(item).to have_attributes(exp: item.exp)
+    end
+  end
 end
--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
-describe User do
-  it {should have_many (:items)}
+RSpec.describe User, type: :model do
+   let(:user) { FactoryGirl.create :user }
+
+   it { is_expected.to validate_presence_of(:email) }
+   it { is_expected.to validate_uniqueness_of(:email) }
+   it { is_expected.to allow_value("user@test.com").for(:email) }
+
+   it { is_expected.to validate_presence_of(:password) }
+   it { is_expected.to validate_length_of(:password).is_at_least(6) }
+
+   describe "attributes" do
+     it "should respond to email" do
+       expect(user).to respond_to(:email)
+     end
+   end
+
+   describe "invalid user" do
+     let(:user_with_invalid_email) { User.new(email: "") }
+     it "should be an invalid user due to blank email" do
+       expect(user_with_invalid_email).to_not be_valid
+     end
+   end
 end
