fa110a1d678a3f700305f5e7647094b91232d5bf
Chris Roper <chris@roper.xyz>
2016-08-08 12:12:35 -0600

    tests fix

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
    let(:user) { FactoryGirl.create :user }
 
+   it {should have_many (:items)}
+
    it { is_expected.to validate_presence_of(:email) }
    it { is_expected.to validate_uniqueness_of(:email) }
    it { is_expected.to allow_value("user@test.com").for(:email) }
